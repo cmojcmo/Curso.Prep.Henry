6d1dce37faa2ad9daf47a279d7fcc8d875aa8bd7
cmojcmo <cmojcmo@gmail.com>
2020-11-16 21:45:17 -0500

    'actualizdos'

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,7 +1,7 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = 'toni';
+const nuevaString = 'juan carlos Marin';
 
 // Crea una variable numérica, puede ser cualquier número:
 const nuevoNum = 2;
@@ -28,12 +28,14 @@ const nuevoModulo = 21 % 5 === 1;
 function devolverString(str) {
   // "Return" la string provista: str
   // Tu código:
+  return str
 }
 
 function suma(x, y) {
   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
   // Tu código:
+  return x + y
 }
 
 function resta(x, y) {
